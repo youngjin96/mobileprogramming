bf0463f6b23a49b13b72eb6c72ecbcbe4c9579f5
jsmme <doyi9917@naver.com>
2022-10-12 17:51:01 +0900

    login success

--- app/src/main/java/com/example/shalendar/AddCalendar.kt
+++ app/src/main/java/com/example/shalendar/AddCalendar.kt
@@ -11,7 +11,7 @@ class AddCalendar :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_calendar)
-
+    // g
 
 
     }
