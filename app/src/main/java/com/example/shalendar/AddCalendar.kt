72119241f3fb9ea5094556159b824467d2e45426
jsmme <doyi9917@naver.com>
2022-10-12 17:15:59 +0900

    로그인 성공 후 페이지 테스트

--- app/src/main/java/com/example/shalendar/AddCalendar.kt
+++ app/src/main/java/com/example/shalendar/AddCalendar.kt
@@ -6,28 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 
-
-
 class AddCalendar :AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_calendar)
 
-        // Note that the Toolbar defined in the layout has the id "my_toolbar"
-//        setSupportActionBar(findViewById(R.id.my_toolbar))
 
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//        findViewById<my_toolbar>(R.id.my_toolbar)
-//            .setupWithNavController(navController, appBarConfiguration)
 
     }
 
 
-
-
-//    val appBarConfiguration = AppBarConfiguration(navController.graph)
-//    val appBarConfiguration = AppBarConfiguration(setOf(R.id.main, R.id.profile))
 
 }
