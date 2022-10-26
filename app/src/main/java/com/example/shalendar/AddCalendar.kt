35940a2855056ec77c9e623dc16d76a38fab8077
jsmme <doyi9917@naver.com>
2022-10-26 17:16:48 +0900

    navbar page connection

--- app/src/main/java/com/example/shalendar/AddCalendar.kt
+++ app/src/main/java/com/example/shalendar/AddCalendar.kt
@@ -56,6 +56,7 @@ class AddCalendar :AppCompatActivity(), NavigationView.OnNavigationItemSelectedL
         }
 
     }
+
 }
 
 
---
+++ app/src/main/java/com/example/shalendar/addgit.java
@@ -0,0 +1,4 @@
+package com.example.shalendar;
+
+public class addgit {
+}
