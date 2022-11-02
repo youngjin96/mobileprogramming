0475c3ba1d828313d1a079acc79f1feab56bad7f
kalmaho <dhsh1214@naver.com>
2022-11-02 20:53:53 +0900

    re

--- controllers/calendar.controller.js
+++ controllers/calendar.controller.js
@@ -19,16 +19,40 @@ exports.calendarSerchAll = (req, res) => {
                 message:
                     err.message || "Some error occurred while retrieving users."
             });
-        else res.send(data);
+            else res.send(data);
     });
 };
-exports.putCalendar = (req, res) => {
-    Calendar.putcalendar((err, data) => {
+exports.createCalendar = (req, res) => {
+    var data = {
+        user_id: req.body.user_id,
+        name : req.body.name,
+        person_num : req.body.person_num,
+    }
+    Calendar.createCalendar(data,(err, data) => {
         if (err)
             res.status(500).send({
                 message:
                     err.message || "Some error occurred while retrieving users."
             });
-        else res.send(data);
+        else 
+        {
+            res.send(data);
+        }
+    });
+};
+exports.deleteCalendar = (req, res) => {
+    var data = {
+        
+    }
+    Calendar.deleteCalendar(data,(err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving users."
+            });
+        else 
+        {
+            res.send(data);
+        }
     });
 };
