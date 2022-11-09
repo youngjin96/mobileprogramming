45025667ce23e5e12f47045ee64981a540e225df
seribianHue <114291733+seribianHue@users.noreply.github.com>
2022-11-09 17:28:24 +0900

    InfoChange kotlin (binding으로 바꿈)

--- app/src/main/java/com/example/shalendar/InfoChange.kt
+++ app/src/main/java/com/example/shalendar/InfoChange.kt
@@ -7,20 +7,21 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.shalendar.databinding.ActivityInfoChangeBinding
 
 
 class InfoChange: AppCompatActivity() {
-    @Override
+    private var mBinding : ActivityInfoChangeBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info_change)
+        mBinding = ActivityInfoChangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button: Button = findViewById<View>(R.id.Personal_info_change_button) as Button
-        button.setOnClickListener {
-            fun onClick(view: View?) {
-                show()
-            }
-        }
+
+
+
 
     }
     fun show() {
@@ -30,12 +31,12 @@ class InfoChange: AppCompatActivity() {
         builder.setPositiveButton(
             "예"
         ) { dialog, which ->
-            Toast.makeText(applicationContext, "예를 선택했습니다.", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "내용을 변경합니다", Toast.LENGTH_LONG).show()
         }
         builder.setNegativeButton(
             "아니오"
         ) { dialog, which ->
-            Toast.makeText(applicationContext, "아니오를 선택했습니다.", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "변경하지 않습니다", Toast.LENGTH_LONG).show()
         }
         builder.show()
     }
