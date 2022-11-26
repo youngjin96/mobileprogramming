dfa3d5d902d02123899126a73cf59cfff35d847c
seribianHue <mongsil99@gmail.com>
2022-11-27 03:57:40 +0900

    InfoChange - 개인적

--- app/src/main/java/com/example/shalendar/InfoChange.kt
+++ app/src/main/java/com/example/shalendar/InfoChange.kt
@@ -1,7 +1,9 @@
 package com.example.shalendar
 
 import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Nickname
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -11,15 +13,35 @@ import com.example.shalendar.databinding.ActivityInfoChangeBinding
 
 
 class InfoChange: AppCompatActivity() {
-    private var mBinding : ActivityInfoChangeBinding? = null
-    private val binding get() = mBinding!!
+    //private var mBinding : ActivityInfoChangeBinding? = null
+   // private val binding get() = mBinding!!
+    private lateinit var binding: ActivityInfoChangeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityInfoChangeBinding.inflate(layoutInflater)
+        binding = ActivityInfoChangeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         binding.PersonalInfoChangeBtn.setOnClickListener(){
-            show()
+            var ID = binding.IDText.text.toString()
+            var Address = binding.AddressText.text.toString()
+            var Nickname = binding.NicknameText.text.toString()
+            var TelNum = binding.TelNumText.text.toString()
+
+            if (ID.isEmpty()) {
+                Toast.makeText(applicationContext, "변경할 아이디를 입력해주십시오", Toast.LENGTH_SHORT).show()
+            }
+            else if (Address.isEmpty()) {
+                Toast.makeText(applicationContext, "변경할 주소를 입력해주십시오", Toast.LENGTH_SHORT).show()
+            }
+            else if (Nickname.isEmpty()) {
+                Toast.makeText(applicationContext, "변경할 닉네임를 입력해주십시오", Toast.LENGTH_SHORT).show()
+            }
+            else if (TelNum.isEmpty()) {
+                Toast.makeText(applicationContext, "변경할 전화번호를 입력해주십시오", Toast.LENGTH_SHORT).show()
+            }
+            else show()
         }
     }
     fun show() {
@@ -30,11 +52,13 @@ class InfoChange: AppCompatActivity() {
             "예"
         ) { dialog, which ->
             Toast.makeText(applicationContext, "내용을 변경합니다", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, AddCalendar::class.java))
         }
         builder.setNegativeButton(
             "아니오"
         ) { dialog, which ->
             Toast.makeText(applicationContext, "변경하지 않습니다", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, AddCalendar::class.java))
         }
         builder.show()
     }
